5a93d0b1eb11e8cfcf9d4db2ad944742f10761ba
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-12-15 15:22:33 +0100

    début des tests
    tests rédigés dans MarkovDataTest

--- src/test/java/markov/MarkovChainTest.java
+++ src/test/java/markov/MarkovChainTest.java
@@ -9,8 +9,28 @@ import markov.MarkovChain;
 public class MarkovChainTest {
 
 	//@Test
-	public void test() {
+	public void test_generateMarkov() {
+		/*String numWord = "1";
+
+		MarkovChain m = new MarkovChain(1);
+
+		String result = m.generateMarkov(1);
+
+		assertEquals(result,numWord);*/
+
 		fail("Not yet implemented");
+
+	}
+
+	//@Test
+	public void test_Learn() {
+		/*String test = "test";
+		MarkovData data = new MarkovData();
+
+		data.read(test);*/
+
+		fail("Not yet implemented");
+
 	}
 
 }
--- src/test/java/markov/MarkovDataTest.java
+++ src/test/java/markov/MarkovDataTest.java
@@ -4,11 +4,35 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MarkovDataTest {
-	
-	//@Test
-	public void test() {
-		fail("Not yet implemented");
+
+	@Test
+	public void test_Read() {
+		MarkovData data = new MarkovData();
+
+		String t = "coucou ça va ?";
+		data.read(t);
+
+		String expected = "coucou ça va ?";
+		String actual = data.getKeyWord(0)+" "+data.getKeyWord(1)+" "+data.getKeyWord(2)+" "+data.getKeyWord(3);
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	public void test_RenforceWord_True() {
+
+		String t = "coucou ça va";
+		MarkovData data = new MarkovData();
+
+		data.learnWord("coucou",t);
+
+		boolean result = data.renforceWord("coucou", t);
+		assertTrue(result);
+
 	}
 
 }
